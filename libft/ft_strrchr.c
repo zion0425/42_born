c1c15859d118b03ddd180a63cd4c0e0788d03a0b
zion0425 <zion0425@naver.com>
2022-01-01 18:24:29 +0900

    split error

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,21 +6,11 @@
 /*   By: siokim <siokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 15:19:06 by siokim            #+#    #+#             */
-/*   Updated: 2022/01/01 17:58:59 by siokim           ###   ########.fr       */
+/*   Updated: 2022/01/01 18:24:12 by siokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -34,15 +24,3 @@ char	*ft_strrchr(const char *s, int c)
 			return ((char *)(s + size));
 	return (0);
 }
-//#include <stdio.h>
-//#include <string.h>
-//int main()
-//{
-//		char *src = electric_alloc(10);
-
-//		__builtin___strcpy_chk (src, "123456789", __builtin_object_size (src, 2 > 1 ? 1 : 0));
-//		ft_strrchr(src, 'a');
-//		src = electric_alloc_rev(10);
-//		__builtin___strcpy_chk (src, "123456789", __builtin_object_size (src, 2 > 1 ? 1 : 0));
-//		ft_strrchr(src, 'a');
-//}
