740e648e2bc16ceb585f26582698f7d1714b4971
zion0425 <zion0425@naver.com>
2022-02-10 20:46:48 +0900

    temp

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: siokim <siokim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/09 19:17:25 by siokim            #+#    #+#             */
+/*   Updated: 2022/02/10 20:26:03 by siokim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+
+int	ft_printf(const char *args, ...);
+
+#endif
