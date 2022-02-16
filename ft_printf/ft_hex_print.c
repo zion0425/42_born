d1e91f13e58bc4bab0dc7257f087d75240089d98
zion0425 <zion0425@naver.com>
2022-02-16 12:15:38 +0900

    finished

--- ft_printf/ft_hex_print.c
+++ ft_printf/ft_hex_print.c
@@ -6,7 +6,7 @@
 /*   By: siokim <siokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 16:35:48 by siokim            #+#    #+#             */
-/*   Updated: 2022/02/14 22:37:13 by siokim           ###   ########.fr       */
+/*   Updated: 2022/02/16 12:15:24 by siokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,37 @@ static size_t	hex_size(unsigned long long str)
 size_t	hex_print(unsigned long long str, char isUpper)
 {
 	char 	*tmp_str;
-	size_t	i;
-	const size_t size = hex_size(str);
+	int	i;
+	size_t	size;
 
+	size = hex_size(str);
 	i = 0;
-	if (isUpper)
-		str = (unsigned long)str;
+	if (isUpper != 2)
+		str = (unsigned int)str;
+	else
+		write(1, "0x", 2);
+	if (str == 0)
+	{
+		write(1, "0" , 1);
+		if (isUpper == 2)
+			return (3);
+		return (1);
+	}
 	tmp_str = malloc(sizeof(char) * (size));
 	if (!tmp_str)
 		return (0);
 	while (str > 0)
 	{
 		if (isUpper == 1)
-			tmp_str[i] = "0123456789ABCDEF"[str % 16];
+			tmp_str[i++] = "0123456789ABCDEF"[str % 16];
 		else
-			tmp_str[i] = "0123456789abcdef"[str % 16];
-		i++;
+			tmp_str[i++] = "0123456789abcdef"[str % 16];
 		str /= 16;
 	}
-	if (isUpper == 2)
-		write(1, "0x", 2);
-	while (tmp_str[--i])
+	while (--i >= 0)
 		write(1, &tmp_str[i], 1);
 	free(tmp_str);
 	if (isUpper == 2)
-		return (unsigned long)(size + 2);
+		size += 2;
 	return (size);
 }
--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,16 +6,19 @@
 /*   By: siokim <siokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 19:17:20 by siokim            #+#    #+#             */
-/*   Updated: 2022/02/13 19:09:25 by siokim           ###   ########.fr       */
+/*   Updated: 2022/02/15 20:46:30 by siokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-size_t	put_str_size(char *s)
+size_t	put_str_size(char *s, char isFree)
 {
+	const size_t	size = ft_strlen(s);
 	ft_putstr_fd(s, 1);
-	return (ft_strlen(s));
+	if(isFree == 1)
+		free(s);
+	return (size);
 }
 
 size_t	put_char_size(char c)
@@ -28,23 +31,21 @@ size_t	identify(va_list ap, const char *str, size_t i)
 {
 	size_t	res_size;
 
-	res_size = 99;
-	if (str[i + 1] == 'd')
-		res_size = put_str_size(ft_itoa(va_arg(ap, int)));
-	if (str[i + 1] == 'i')
-		res_size = put_str_size(ft_itoa(va_arg(ap, int)));
+	res_size = 0;
+	if (str[i + 1] == 'd' || str[i + 1] == 'i')
+		res_size = put_str_size(ft_itoa(va_arg(ap, int)), 1);
 	if (str[i + 1] == 'c')
 		res_size = put_char_size(va_arg(ap, int));
 	if (str[i + 1] == 's')
-		res_size = put_str_size(va_arg(ap, char *));
+		res_size = put_str_size(va_arg(ap, char *), 0);
 	if (str[i + 1] == 'u')
-		res_size = put_str_size(unsigned_ft_itoa(va_arg(ap, unsigned int)));
+		res_size = put_str_size(unsigned_ft_itoa(va_arg(ap, unsigned int)), 1);
 	if (str[i + 1] == '%')
 		res_size = put_char_size('%');
-	if (str[i + 1] == 'x')
-		res_size = hex_print(va_arg(ap, unsigned int), 0);
-	if (str[i + 1] == 'X')
-		res_size = hex_print(va_arg(ap, unsigned int), 1);
+	if (str[i + 1] == 'x' || str[i + 1] == 'X')
+		res_size = hex_print(va_arg(ap, unsigned int), (str[i + 1] == 'X'));
+	//if (str[i + 1] == 'X')
+	//	res_size = hex_print(va_arg(ap, unsigned long long), 1);
 	if (str[i + 1] == 'p')
 		res_size = hex_print(va_arg(ap, unsigned long long), 2);
 	return (res_size);
@@ -66,55 +67,22 @@ int	ft_printf(const char *str, ...)
 	while (str[i])
 	{
 		if (str[i] == '%')
-			res_size += identify(ap, str, i++) - 1;
+			res_size += identify(ap, str, i++);
 		else
+		{
 			write(1, &str[i], 1);
+			res_size += 1;
+		}
 		i++;
 	}
-	return (res_size + i - 1);
+	return (res_size);
 }
 
-#include <stdio.h>
+//#include <stdio.h>
 
-int main()
-{
-	//char *a = "AB";
-	int	a = -122;
-	//char	a = 'A';
+//int main()
+//{
+//	printf("\nhi : %d",printf(" %x ", 0));
+//	printf("\nhi : %d", ft_printf(" %x ", 0));
 
-	//%d는 10진수 숫자를 출력합니다.
-	//printf("\nhi : %d", printf("%d\n",a));
-	//printf("\nhi : %d", ft_printf("%d\n", a));
-
-	////%i는 10진수 정수를 출력합니다.
-	//printf("\nhi : %d", printf("%i\n",a));
-	//printf("\nhi : %d", ft_printf("%i\n", a));
-
-	////%c는 단일 문자 (character) 한 개를 출력합니다.
-	//printf("\nhi : %d", printf("%c\n",a));
-	//printf("\nhi : %d", ft_printf("%c\n", a));
-
-	////%s는 문자열 (string) 을 출력합니다.
-	//printf("\nhi : %d", printf("a%s\n",a));
-	//printf("\nhi : %d", ft_printf("a%s\n", a));
-
-	////%u는 부호 없는 10진수 숫자를 출력합니다.
-	//printf("\nhi : %d", printf("a%u\n",a));
-	//printf("\nhi : %d", ft_printf("a%u\n", a));
-
-	////%%는 퍼센트 기호 (%) 를 출력합니다.
-	//printf("\nhi : %d", printf("a%%\n"));
-	//printf("\nhi : %d", ft_printf("a%%\n"));
-
-	////%x는 소문자를 사용하여 숫자를 16진수로 출력합니다.
-	//printf("\nhi : %d", printf("a%x\n",a));
-	//printf("\nhi : %d", ft_printf("a%x\n", a));
-
-	////%X는 대문자를 사용하여 숫자를 16진수로 출력합니다.
-	//printf("\nhi : %d", printf("a%X\n",a));
-	//printf("\nhi : %d", ft_printf("a%X\n", a));
-
-	//%p는 void * 형식의 포인터 인자를 16진수로 출력합니다.
-	printf("\nhi : %d", printf("a%p\n",&a));
-	printf("\nhi : %d", ft_printf("a%p\n", &a));
-}
+//}
