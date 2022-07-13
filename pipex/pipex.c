40525f2a51c49ccda8e1cdb99236ce0a39647ce7
zion0425 <zion0425@naver.com>
2022-07-13 17:20:18 +0900

    m

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,21 +6,112 @@
 /*   By: siokim <siokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 19:39:13 by siokim            #+#    #+#             */
-/*   Updated: 2022/06/27 18:34:17 by siokim           ###   ########.fr       */
+/*   Updated: 2022/07/13 17:17:56 by siokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-#include <stdio.h>
 
-int	main(int argc, char **argv)
+void	print_error(char *filename)
 {
+	perror(filename);
+	exit(1);
+}
 
-    char *envp[] = {0};
+int	openfile(char *filename, char format)
+{
+	int	fdin;
+	int	fdout;
 
-	execve("/bin/", argv, envp);
+	if (format == INFILE)
+	{
+		if (access(filename, F_OK) == -1)
+			print_error(filename);
+		fdin = open(filename, O_RDONLY);
+		if (fdin == -1)
+			print_error(filename);
+		return (fdin);
+	}
+	else
+	{
+		fdout = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0755);
+		if (fdout == -1)
+			print_error(filename);
+		return (fdout);
+	}
+}
 
-	if (argc < 5)
-		return (0);
+char	*getpath(char **envp, char *command)
+{
+	int		i;
+	char	*path;
+	char	**paths;
+	char	*tmp2;
 
+	i = -1;
+	while (envp[++i] != NULL)
+		if (!ft_strncmp(envp[i], "PATH=", 5))
+			break ;
+	if (envp[i] == NULL)
+		return (command);
+	path = &envp[i][5];
+	paths = ft_split(path, ':');
+	i = -1;
+	while (paths[++i])
+	{
+		tmp2 = ft_strjoin(ft_strjoin(paths[i], "/"), command);
+		if (!access(tmp2, F_OK))
+			return (tmp2);
+		free(tmp2);
+	}
+	print_error(command);
+	return (0);
+}
+
+void	pipex(char **envp, char *command)
+{
+	int		pipefd[2];
+	int		pid;
+	char	*path;
+	char	**cmds;
+
+	pipe(pipefd);
+	pid = fork();
+	if (pid > 0)
+	{
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN_FILENO);
+		waitpid(pid, 0, 0);
+	}
+	else if (pid == 0)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		cmds = ft_split(command, ' ');
+		path = getpath(envp, cmds[0]);
+		execve(path, cmds, envp);
+	}
+	else
+		print_error("fork_error");
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*path;
+	char	**argvs;
+	int		fdin;
+	int		fdout;
+
+	if (argc == 5)
+	{
+		fdin = openfile(argv[1], INFILE);
+		fdout = openfile(argv[4], OUTFILE);
+		dup2(fdin, STDIN_FILENO);
+		dup2(fdout, STDOUT_FILENO);
+		pipex(envp, argv[2]);
+		argvs = ft_split(argv[3], ' ');
+		path = getpath(envp, argvs[0]);
+		execve(path, argvs, envp);
+	}
+	return (1);
 }
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,16 +6,25 @@
 /*   By: siokim <siokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 19:39:10 by siokim            #+#    #+#             */
-/*   Updated: 2022/06/25 19:47:09 by siokim           ###   ########.fr       */
+/*   Updated: 2022/07/13 12:50:52 by siokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef PIPEX_H
 # define PIPEX_H
-# include <unistd.h>
-# include <stdlib.h>
+# define INFILE 0
+# define OUTFILE 1
+# define INPUT_ERROR -1
 
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+char	**ft_split(char const *s, char c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
-
